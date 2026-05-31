aceb8a55771d35fb73dec460584d5ef9ae1039d1
Yap <y.kang73727@yahoo.com>
2026-05-31 20:53:48 +0800

    Add favicon

--- includes/layout.php
+++ includes/layout.php
@@ -9,6 +9,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $page_title ?? 'Trainer Database'; ?></title>
+    <link rel="icon" type="image/svg+xml" href="favicon.svg">
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
